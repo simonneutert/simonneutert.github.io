9e58a5bc0740786ca2b9f6a288919d6cb94498b6
Simon Neutert <simonneutert@users.noreply.github.com>
2025-11-03 18:24:01 +0100

    Enhance navbar accessibility by adding roles and aria-labels; refactor current_page? method for clarity

--- src/_components/shared/navbar.rb
+++ src/_components/shared/navbar.rb
@@ -2,4 +2,8 @@ class Shared::Navbar < Bridgetown::Component
   def initialize(metadata:, resource:)
     @metadata, @resource = metadata, resource
   end
+
+  def current_page?(path)
+    @resource.relative_url == path || @resource.relative_url == "#{path}/"
+  end
 end
